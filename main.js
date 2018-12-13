6a9c8eac088f573a85e632e6c73962cc0e6d88cc
Alex Hval <alexhval@juno.com>
2018-12-13 12:24:47 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+$('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    touchThreshold: 20,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+
+  $('.slider-nav').slick({
+    slidesToShow: 1.25,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    swipeToSlide: true,
+    dots: false,
+    centerMode: true,
+    focusOnSelect: true
+  });
